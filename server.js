a8ad1595ecbbe930d9befaa9e9d059074397beac
Synce <hotlos.jakub@gmail.com>
2018-06-09 21:59:10 +0200

    a

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path")
 
 
 const express = require("express")
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 
 var app = express();
 app.use(express.static('public'))
