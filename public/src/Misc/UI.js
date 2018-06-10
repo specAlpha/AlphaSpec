9cd3985447db44472802cc0c6c2ab3ce9897effa
Synce <hotlos.jakub@gmail.com>
2018-06-10 19:28:35 +0200

    some Changes

--- public/src/Misc/UI.js
+++ public/src/Misc/UI.js
@@ -1,26 +1,26 @@
 class UI {
     constructor() {
         this.canvas3d = document.createElement('canvas')
-        this.canvas3d.width = 100;
-        this.canvas3d.height = 50;
+        this.canvas3d.width = 1536;
+        this.canvas3d.height = 1024;
         this.timerDiv = $('#timer');
 
         this.texture = new THREE.CanvasTexture(this.canvas3d)
-        this.plane = new Plane(new THREE.Vector3(-25, 20, 20), new THREE.Vector3(30, 20, 0), new THREE.Euler(0, -Math.PI / 8, 0), '', false,
+        this.plane = new Plane(new THREE.Vector3(-25, 20, 20), new THREE.Vector3(30, 20, 0), new THREE.Euler(0, Math.PI + -Math.PI / 8, 0), '', false,
             new THREE.MeshBasicMaterial({
                 map: this.texture,
-                side: THREE.DoubleSide,
+                side: THREE.FrontSide,
                 opacity: 1,
                 transparent: true
             })
         )
-        this.timerHelper = new TimerHelper(5, 5)
+        this.timerHelper = new TimerHelper(15, 15)
 
     }
 
     setPlaneImg(image) {
         this.timerHelper.running = true;
-        this.canvas3d.getContext('2d').drawImage(GM.textureBank.getImage(image), 0, 0, 100, 50)
+        this.canvas3d.getContext('2d').drawImage(GM.textureBank.getImage(image), 0, 0, this.canvas3d.width, this.canvas3d.height)
         this.texture.needsUpdate = true;
 
     }
@@ -28,7 +28,7 @@ class UI {
     update(time) {
         if (this.timerHelper.running) {
 
-            this.plane.material.opacity = 5 - this.timerHelper.update(time)
+            this.plane.material.opacity = 15 - this.timerHelper.update(time)
             if (this.timerHelper.complete) {
                 this.timerHelper.reset();
 
