532943a8b3f4ae7b96309b0027515c6eb03a5c82
ziobrowskyy <thomas.ziobrowski@gmail.com>
2018-06-13 09:52:36 +0200

    Działa import
    Musze dodac wiecej klas

--- public/levelEditor/Shape.js
+++ public/levelEditor/Shape.js
@@ -1,107 +1,164 @@
 class Shape {
-   constructor(_type, _size, _position, _rotation, _special, _id) {
-      // type validation
-      if (!(_type == "cube" || _type == "ramp" || _type == "plane" || _type == "player")) {
-         console.log("typ " + _type);
-         return;
+   constructor(_pos, _rot) {
+      if (_pos && _rot) {
+         this.position = {
+            x: _pos.x || $("#posX").val() || 0,
+            y: _pos.y || $("#posX").val() || 0,
+            z: _pos.z || $("#posX").val() || 0
+         };
+         this.rotation = {
+            x: _rot.x || $("#rotX").val() || 0,
+            y: _rot.y || $("#rotY").val() || 0,
+            z: _rot.z || $("#rotZ").val() || 0
+         };
+      } else {
+         this.position = {
+            x: $("#posX").val() || 0,
+            y: $("#posX").val() || 0,
+            z: $("#posX").val() || 0
+         };
+         this.rotation = {
+            x: $("#rotX").val() || 0,
+            y: $("#rotY").val() || 0,
+            z: $("#rotZ").val() || 0
+         };
       }
-      this.type = _type;
-      this.special = _special || false;
-      this.id = _id || this.special ? specialCount++ : null;
-      this.name;
-      this.div;
-      this.size = {
-         x: $("#sizeX").val() || 10,
-         y: $("#sizeY").val() || 10,
-         z: $("#sizeZ").val() || 10
-      };
-      this.rotation = {
-         x: $("#rotX").val() || 0,
-         y: $("#rotY").val() || 0,
-         z: $("#rotZ").val() || 0
-      };
-      this.position = {
-         x: $("#posX").val() || 0,
-         y: $("#posX").val() || 0,
-         z: $("#posX").val() || 0
-      };
-
+      this.props = {
+         position: this.position,
+         rotation: this.rotation,
+      }
       this.material = new THREE.MeshBasicMaterial({
          color: 0x123123,
          opacity: 1,
          side: THREE.DoubleSide,
       });
       this.texture = $("#materialSelect").val() || "default";
-      this.geometry = this.getGeometry();
-      this.mesh = new THREE.Mesh(this.geometry, this.material);
-      console.log(this.mesh);
-      scene.add(this.mesh);
-
-      objects.push(this);
 
       this.div = document.createElement('div');
-      this.div.innerHTML = this.type;
+      this.div.innerHTML = "default";
       this.div.parent = this;
-      $(this.div).on('click', this.edit);
+      $(this.div).on('click', this.edit.bind(this));
       $(this.div).addClass('option');
       $(this.div).appendTo($('#list'));
-      return this;
+      objects.push(this);
    }
-
+   applyDimensions() {
+      console.log(this.position)
+      this.mesh.position.set(this.position.x, this.position.y, this.position.z)
+   }
+   delete() {
+      $(this.div).remove();
+      scene.remove(this.mesh);
+      let index = objects.indexOf(this);
+      objects.splice(index, index + 1);
+   }
    edit() {
       if (currSel) $(currSel.div).toggleClass("check")
-
-      $(this).toggleClass("check");
-      currSel = this.parent;
-      $('#posX').val(this.parent.position.x);
-      $('#posY').val(this.parent.position.y);
-      $('#posZ').val(this.parent.position.z);
-      $('#rotX').val(this.parent.rotation.x);
-      $('#rotY').val(this.parent.rotation.y);
-      $('#rotZ').val(this.parent.rotation.z);
-      $('#posZ').val(this.parent.position.z);
-      $('#sizeX').val(this.parent.size.x);
-      $('#sizeY').val(this.parent.size.y);
-      $('#sizeZ').val(this.parent.size.z);
-   }
-   udapteRotatoin(axis, value) {
-      this.rotation[axis] = value;
-   }
-
-   getGeometry() {
-      let geometry;
-      switch (this.type) {
-         case ("ramp"): {
-            let shape = new THREE.Shape();
-            shape.moveTo(0, 0);
-            shape.lineTo(this.size.x, this.size.y);
-            shape.lineTo(this.size.x, 0);
-            shape.lineTo(0, 0);
-            geometry = new THREE.ExtrudeGeometry(shape, {
-               steps: 1,
-               amount: this.size.z,
-               bevelEnabled: false,
-               bevelThickness: 1,
-               bevelSize: 1,
-               bevelSegments: 2
-            });
-         }
-            break;
-
-         case ("player"): {
-            console.log(this.size)
-            geometry = new THREE.BoxGeometry(this.size.x, this.size.y, this.size.z);
-         }
-            break;
-
-         case ("plane"): {
-            geometry = new THREE.PlaneGeometry(this.size.x, this.size.y, this.size.z)
-         }
-         case ("cube"): {
-            geometry = new THREE.BoxGeometry(this.size.x, this.size.y, this.size.z)
-         }
-            break;
+      $(this.div).toggleClass("check");
+      currSel = this;
+      $('#posX').val(this.position.x);
+      $('#posY').val(this.position.y);
+      $('#posZ').val(this.position.z);
+      $('#rotX').val(this.rotation.x);
+      $('#rotY').val(this.rotation.y);
+      $('#rotZ').val(this.rotation.z);
+      $('#posZ').val(this.position.z);
+      if (this.size) {
+         $('#sizeX').val(this.size.x);
+         $('#sizeY').val(this.size.y);
+         $('#sizeZ').val(this.size.z);
       }
-      return geometry;
    }
 }
+class Player extends Shape {
+   constructor(_pos, _rot, _nr) {
+      let playerNumber = _nr || objects.filter(e => e.constructor.name == "Player").length;
+      super(_pos, _rot)
+      this.type = "player";
+      this.name = `player${playerNumber}`
+      this.geometry = new THREE.BoxGeometry(10, 20, 10);
+      this.mesh = new THREE.Mesh(this.geometry, this.material);
+      this.div.innerHTML = this.constructor.name;
+      this.applyDimensions();
+      if (playerNumber >= 2) {
+         this.mesh = null;
+         this.delete();
+         console.log("Nie może być więcje niż dwóch graczy!")
+      }
+   }
+}
+
+class Plane extends Shape {
+   constructor(_pos, _rot, _size) {
+      super(_pos, _rot)
+      this.type = "staticBlock";
+      if (_size) {
+         this.size = {
+            x: $("#sizeX").val() || 10,
+            y: $("#sizeY").val() || 10,
+            z: $("#sizeZ").val() || 10
+         }
+
+      } else {
+
+      }
+      this.props.size = this.size;
+      this.geometry = new THREE.PlaneGeometry(this.size.x, this.size.y, this.size.z);
+      this.mesh = new THREE.Mesh(this.geometry, this.material);
+      this.div.innerHTML = this.constructor.name;
+      this.applyDimensions();
+
+
+   }
+}
+class Cube extends Shape {
+   constructor(_pos, _rot, _size) {
+      super(_pos, _rot)
+      this.type = "staticBlock";
+      this.size = {
+         x: $("#sizeX").val() || 10,
+         y: $("#sizeY").val() || 10,
+         z: $("#sizeZ").val() || 10
+      }
+      this.props.size = this.size;
+      this.geometry = new THREE.BoxGeometry(this.size.x, this.size.y, this.size.z);
+      this.mesh = new THREE.Mesh(this.geometry, this.material);
+      this.div.innerHTML = this.constructor.name;
+      this.applyDimensions();
+
+   }
+}
+class Ramp extends Shape {
+   constructor(_pos, _rot, _size) {
+      super(_pos, _rot)
+      this.type = "staticBlock";
+      this.size = {
+         x: $("#sizeX").val() || 10,
+         y: $("#sizeY").val() || 10,
+         z: $("#sizeZ").val() || 10
+      };
+      this.props.size = this.size;
+      let shape = new THREE.Shape();
+      shape.moveTo(0, 0);
+      shape.lineTo(this.size.x, this.size.y);
+      shape.lineTo(this.size.x, 0);
+      shape.lineTo(0, 0);
+      this.geometry = new THREE.ExtrudeGeometry(shape, {
+         steps: 1,
+         amount: this.size.z,
+         bevelEnabled: false,
+         bevelThickness: 1,
+         bevelSize: 1,
+         bevelSegments: 2
+      });
+      this.mesh = new THREE.Mesh(this.geometry, this.material); this.div.innerHTML = this.constructor.name;
+      this.applyDimensions();
+
+   }
+}
+// class dynamicCube extends Shape {
+//    constructor() {
+//       super()
+//       this.type = "dynamicTile"
+//    }
+// }
--- public/levelEditor/main.js
+++ public/levelEditor/main.js
@@ -1,6 +1,8 @@
 //todo
 //skonczyc import
 //dynamic cubes
+//wybor elementow do dodania z listy
+//ogarnac jebany burdel
 
 let renderer, camera, scene, axes;
 let currSel, prevSel;
@@ -16,32 +18,27 @@ Math.degrees = function (radians) {
 }
 
 $(function () {
-
+   // useless stuff
    camera = new THREE.PerspectiveCamera(45, 1500 / 700, 1, 20000);
-
    camera.position.set(400, 400, 400);
-
    scene = new THREE.Scene();
    let grid = new THREE.GridHelper(20000, 2000, 0x000000, 0x000000);
    grid.material.opacity = 0.2;
    grid.material.transparent = true;
    scene.add(grid);
-
-
    axes = new THREE.AxesHelper(10000)
-
-
    scene.background = new THREE.Color(0xa0a0a0);
    renderer = new THREE.WebGLRenderer();
    renderer.setSize(1299, 699)
-
    var orbitControl = new THREE.OrbitControls(camera, renderer.domElement);
    orbitControl.addEventListener('change', function () {
       renderer.render(scene, camera)
    });
    $("#root").append(renderer.domElement);
 
-   // axes
+
+   // end of useless stuff
+
    let axesAdded = true
    $('#axes').on('click', function () {
       $('#axes').toggleClass('check');
@@ -49,27 +46,23 @@ $(function () {
       axesAdded ? scene.remove(axes) : scene.add(axes);
    })
 
+   $("#newPlayer").on("click", () => {
+      let temp = new Player();
+      scene.add(temp.mesh);
+   })
+
    $("#newCube").on("click", () => {
-      let temp = new Shape("cube");
+      let temp = new Cube();
+      scene.add(temp.mesh);
    });
 
-   $("#newPlayer").on("click", () => {
-      let playerNumber = objects.filter(e => e.type == "player").length;
-      if (playerNumber >= 2) {
-         window.alert("Nie może być więcej niż dwóch graczy!");
-         return;
-      }
-      let temp = new Shape("player");
-      objects[objects.indexOf(temp)].name = `player${playerNumber}`
-   })
    $("#newPlane").on("click", () => {
-      let temp = new Shape("plane");
+      let temp = new Plane();
+      scene.add(temp.mesh);
    })
    $("#newRamp").on("click", () => {
-      let temp = new Shape("ramp");
-   })
-   $("#newPressurePlate").on("click", () => {
-      let temp = new Shape("pressurePlate");
+      let temp = new Ramp();
+      scene.add(temp.mesh);
    })
 
 
@@ -78,78 +71,58 @@ $(function () {
          window.alert("Musisz najpierw wybrać obiekt do usunięcia!")
          return null;
       }
-      $(currSel.div).remove();
-      scene.remove(currSel.mesh)
-      let index = objects.indexOf(currSel);
-      objects.splice(index, index + 1);
-
+      currSel.delete();
+      currSel = null;
    })
 
    $('#posX').on('input', function () {
       currSel.mesh.position.x = $(this).val();
+      currSel.position.x = $(this).val();
    })
    $('#posZ').on('input', function () {
       currSel.mesh.position.z = $(this).val();
+      currSel.position.z = $(this).val();
    })
    $('#posY').on('input', function () {
       currSel.mesh.position.y = $(this).val();
+      currSel.position.y = $(this).val();
    })
 
 
    $('#rotX').on('input', function () {
       currSel.mesh.rotation.x = Math.radians($(this).val());
+      currSel.rotation.x = Math.radians($(this).val());
    })
    $('#rotY').on('input', function () {
       currSel.mesh.rotation.z = Math.radians($(this).val());
+      currSel.rotation.y = Math.radians($(this).val());
    })
    $('#rotZ').on('input', function () {
       currSel.mesh.rotation.y = Math.radians($(this).val());
+      currSel.rotation.z = Math.radians($(this).val());
    })
 
 
 
 
    $("#output").on("click", () => {
-      exported = {
-         spawn: {
-
-         },
-         staticBlocks: {},
-
-      }
+      exported = {}
       for (let i = 0; i < objects.length; i++) {
          let el = objects[i];
 
          if (el.type == "player") {
-            exported.spawn[el.name] = {
-               position: el.position,
-               rotation: el.rotation
-
-            }
+            if (!exported.spawn)
+               exported.spawn = {}
+            exported.spawn[el.name] = el.props;
          }
-         else if (!el.special) {
-            if (!exported.staticBlocks[el.type]) {
-               exported.staticBlocks[el.type] = [];
-            }
-            exported.staticBlocks[el.type].push({
-               position: el.position,
-               rotation: el.rotation,
-               size: el.size
-            })
-         } else if (el.special) {
-            if (!exported.specialTiles[el.type]) {
-               exported.specialTiles[el.type] = [];
-            }
-            exported.specialTiles[el].push({
-               position: el.position,
-               rotation: el.rotation,
-               id: el.id
-            });
+         else {
+            if (!exported[el.type])
+               exported[el.type] = {};
+            if (!exported[el.type][el.constructor.name])
+               exported[el.type][el.constructor.name] = []
+            exported[el.type][el.constructor.name].push(el.props)
          }
-
-
       }
-
       copyToClipboard(JSON.stringify(exported));
    })
 
@@ -164,4 +137,43 @@ $(function () {
 
 function copyToClipboard(text) {
    window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+}
+
+function level() {
+   data = `{"spawn":{"player0":{"position":{"x":"150","y":"10","z":"50"},"rotation":{"x":0,"y":0,"z":0}}},"staticBlock":{"Cube":[{"position":{"x":"150","y":"150","z":"150"},"rotation":{"x":"0","y":"0","z":"0"},"size":{"x":"50","y":"50","z":"200"}}],"Plane":[{"position":{"x":"1","y":"150","z":"150"},"rotation":{"x":"0","y":"0","z":"0"},"size":{"x":"50","y":"50","z":"200"}}]}}`
+   data = JSON.parse(data)
+   for (let i = objects.length - 1; i >= 0; i--) objects[i].delete();
+   for (let type in data) {
+      console.log(type)
+      switch (type) {
+         case ("spawn"):
+            for (let player in data.spawn) {
+               let el = data.spawn[player];
+               let temp = new Player(el.position, el.rotation, player[player.length - 1]);
+               scene.add(temp.mesh)
+            }
+            break;
+         case ("staticBlock"):
+            for (let subType in data.staticBlock) {
+               for (let obj in data.staticBlock[subType]) {
+                  let el = data.staticBlock[subType][obj];
+                  let temp;
+                  console.log(el)
+                  if (subType == "Ramp") {
+                     temp = new Ramp(el.position, el.rotation, el.size)
+                  } else if (subType == "Plane") {
+                     temp = new Plane(el.position, el.rotation, el.size)
+                  } else if (subType == "Cube") {
+                     temp = new Cube(el.position, el.rotation, el.size)
+                  } else {
+                     console.log(subType)
+                  }
+                  scene.add(temp.mesh);
+               }
+            }
+            break;
+         case ("dynamicTile"):
+            break;
+      }
+   }
 }
